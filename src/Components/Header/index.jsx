de6524bf6499a2200e88b3f3723150219576ae0c
Alexander Tejedor <atromero.98@gmail.com>
2025-03-13 19:26:59 -0500

    fix(images): I move the images to public directory and create a new Components directory

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header>
+            <figure className='w-full py-7 px-8'>
+                <img className='w-25' src="/images/logo.svg" alt="Base Apparel logo" />
+            </figure>
+        </header>
+    )
+}
+
+export { Header }
---
+++ src/pages/App/index.jsx
@@ -0,0 +1,14 @@
+import { Header } from '../../Components/Header'
+import { Main } from '../../Components/Main'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
+}
+
+export { App }
